087f0ef22b5303841b4138e5b52a7669387d8ea7
guilhermeabel99 <gpeuvis@gmail.com>
2020-12-26 20:12:38 -0300

    1 test passed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+      api.get('repositories').then(response => {
+        setRepositories(response.data);
+           });
+
+   },    []);
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Umbriel',
+      url: 'https://github.com/guilhermeabel99',
+      techs: ['Node.js', 'ReactJS']
+    })
+
+    setRepositories([ ...repositories, response.data ]);
+    
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +32,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+            <li key={repository.id}>
+          {repository.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
